8e6bd4843fb45750de0b9f09462a9d3157b9623d
marzelin <marzelin@gmail.com>
2017-12-28 14:03:51 +0100

    (struct) move app styles to separate files

--- src/Components/App.tsx
+++ src/Components/App.tsx
@@ -1,50 +1,18 @@
-import { injectGlobal } from "emotion";
 import * as React from "react";
-import styled from "react-emotion";
 import { Provider } from "react-redux";
-import createStore from "../store";
+import "../globalStyles";
+import getStore from "../store";
 import Buttons from "./Buttons";
 import Indicators from "./Indicators";
 import InputGetter from "./InputGetter";
 import Masthead from "./Masthead";
 import QuoteBox from "./QuoteBox";
-
-// tslint:disable-next-line:no-unused-expression
-injectGlobal`
-  html {
-    font-size: 14px;
-  }
-  body {
-    font-family: -apple-system, BlinkMacSystemFont, "Segoe UI", Roboto, Oxygen,
-    Ubuntu, Cantarell, "Fira Sans", "Droid Sans", "Helvetica Neue", Arial, sans-serif,
-    "Apple Color Emoji", "Segoe UI Emoji", "Segoe UI Symbol";
-    padding: 0;
-    margin: 0;
-    font-size: 14px;
-    color: #39464E;
-  }
-`;
-
-const Container = styled("div")`
-  margin-left: 1em;
-  margin-right: 1em;
-  @media only screen and (min-width: 768px) {
-    width: 723px;
-    margin-left: auto;
-    margin-right: auto;
-  }
-  @media only screen and (min-width: 992px) {
-    width: 933px;
-  }
-  @media only screen and (min-width: 1200px) {
-    width: 1127px;
-  }
-`;
+import Container from "./Views/AppContainer";
 
 const Void = ({ children }: { children: React.ReactNode }): JSX.Element =>
   children as JSX.Element;
 
-const store = createStore();
+const store = getStore();
 
 export default () => (
   <Provider store={store}>
---
+++ src/globalStyles.tsx
@@ -0,0 +1,17 @@
+import { injectGlobal } from "emotion";
+
+// tslint:disable-next-line:no-unused-expression
+injectGlobal`
+  html {
+    font-size: 14px;
+  }
+  body {
+    font-family: -apple-system, BlinkMacSystemFont, "Segoe UI", Roboto, Oxygen,
+    Ubuntu, Cantarell, "Fira Sans", "Droid Sans", "Helvetica Neue", Arial, sans-serif,
+    "Apple Color Emoji", "Segoe UI Emoji", "Segoe UI Symbol";
+    padding: 0;
+    margin: 0;
+    font-size: 14px;
+    color: #39464E;
+  }
+`;
