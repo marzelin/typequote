7cbecc2cfc8a4d28bcd2a0dff8500a418e049c34
marzelin <marzelin@gmail.com>
2017-12-30 18:13:48 +0100

    (tidy) tidy actionCreators

--- src/actionCreators.ts
+++ src/actionCreators.ts
@@ -1,10 +1,12 @@
 import { Idispatch, TgetState } from "redux";
 import Action from "./actions";
+
 export const checkIfCorrect = (char: string) => (
   dispatch: Idispatch,
   getState: TgetState
 ) => {
   const { current, text: [text] } = getState();
+
   if (text[current] === char) {
     dispatch(inputCorrect);
     if (current === text.length - 1) {
@@ -15,16 +17,16 @@ export const checkIfCorrect = (char: string) => (
   }
 };
 
-type AC = () => Action;
-type AC1<T> = (arg: T) => Action;
+export const startTyping: AC = () => ({ type: "TYPING_STARTED" });
+export const newQuote: AC = () => ({ type: "NEW_QUOTE" });
 
 const typingCompleted: Action = { type: "TYPING_COMPLETED" };
 const inputCorrect: Action = { type: "INPUT_CORRECT" };
-
-export const startTyping: AC = () => ({ type: "TYPING_STARTED" });
-export const newQuote: AC = () => ({ type: "NEW_QUOTE" });
 
 const inputIncorrect: AC1<number> = index => ({
   payload: index,
   type: "INPUT_INCORRECT"
 });
+
+type AC = () => Action;
+type AC1<T> = (arg: T) => Action;
